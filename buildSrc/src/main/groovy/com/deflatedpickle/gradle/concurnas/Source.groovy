95332ab1aa442bdb2f0221e05065185b5e3c1dfd
DeflatedPickle <DeflatedPickle@gmail.com>
2020-01-23 06:22:58 +0000

    Update Source.groovy

--- buildSrc/src/main/groovy/com/deflatedpickle/gradle/concurnas/Source.groovy
+++ buildSrc/src/main/groovy/com/deflatedpickle/gradle/concurnas/Source.groovy
@@ -3,10 +3,6 @@ package com.deflatedpickle.gradle.concurnas
 class Source {
     // The compilers directory
     String compiler
-    // The source folder
-    String src = './src/main/conc'
-    // The compiler version - unused
-    String version = '1.14.016'
     // The JVM arguments
     String[] jArgs = []
     // The Concurnas arguments
